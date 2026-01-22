4ed873548455dd84e2b55524cb9f458116dac922
Eli Romano Salas <75858128+Eli676@users.noreply.github.com>
2026-01-21 19:22:14 -0600

    Remove site URL from astro.config.mjs
    Remove site URL from Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({ 
-  site: 'https://Eli676.github.io',
   vite: {
       plugins: [tailwindcss()],
   },
